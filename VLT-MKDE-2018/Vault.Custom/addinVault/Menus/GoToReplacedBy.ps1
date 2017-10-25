427875dd5282a098337d23dcb3988a65b12f640e
koechlm <markus.koechl@autodesk.com>
2017-10-25 20:08:47 +0200

    updated GoToSource /Replacement by 2017-10-25 AKN verision

--- VLT-MKDE-2018/Vault.Custom/addinVault/Menus/GoToReplacedBy.ps1
+++ VLT-MKDE-2018/Vault.Custom/addinVault/Menus/GoToReplacedBy.ps1
@@ -10,6 +10,9 @@
 #=============================================================================#
 #endregion
 
+#read UIStrings (only VDS dialogs/tabs do this as a default)
+$UIString = mGetUIStrings
+
 #region variables
 # change the quoted display name according your Vault Property Definition
 	$_ReplacedByFilePropDispName = "Ersetzt durch"
@@ -28,60 +31,61 @@ if ($currentSelected.TypeId.EntityClassId -eq "FILE")
 $mLatestFile = $vault.DocumentService.GetLatestFileByMasterId($mFileMasterId)
 
 #get properties of selected file
-
-$PropDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
-$propDefIds = @()
-$PropDefs | ForEach-Object {
-	$propDefIds += $_.Id
-} 
-$mPropDef = $propDefs | Where-Object { $_.DispName -eq $_ReplacedByFilePropDispName}
-	
-$mEntIDs = @()
-$mEntIDs += $mLatestFile.Id
-$mPropDefIDs = @()
-$mPropDefIDs += $mPropDef.Id
-$mProp = $vault.PropertyService.GetProperties("FILE",$mEntIDs, $mPropDefIDs)
-
-$mVal = $mProp[0].Val
-
-If ($mVal -eq '--') 
-		{ 
-			[System.Windows.MessageBox]::Show("Es ist kein Ersatz definiert.", "Vault Datei-Extras")
-			return
-		}
+Try{
+	$PropDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
+	$propDefIds = @()
+	$PropDefs | ForEach-Object {
+		$propDefIds += $_.Id
+	} 
+	$mPropDef = $propDefs | Where-Object { $_.DispName -eq $_ReplacedByFilePropDispName}
+	$mEntIDs = @()
+	$mEntIDs += $mLatestFile.Id
+	$mPropDefIDs = @()
+	$mPropDefIDs += $mPropDef.Id
+	$mProp = $vault.PropertyService.GetProperties("FILE",$mEntIDs, $mPropDefIDs)
+	$mVal = $mProp[0].Val
+	IF(-not $mVal){
+		[System.Windows.MessageBox]::Show([String]::Format($UIString["ADSK-GoToSource_MSG00"], $_ReplacedByFilePropDispName), $UIString["ADSK-GoToSource_MNU00"])
+		return
+	}
+}
+Catch{
+	[System.Windows.MessageBox]::Show([String]::Format($UIString["ADSK-GoToSource_MSG00"], $_ReplacedByFilePropDispName), $UIString["ADSK-GoToSource_MNU00"])
+	return
+}
 
 $srchConds = New-Object autodesk.Connectivity.WebServices.SrchCond[] 1
-		$srchCond = New-Object autodesk.Connectivity.WebServices.SrchCond
-		$propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
-		$propNames = @("Name")
-		$propDefIds = @{}
-		foreach($name in $propNames) {
-			$propDef = $propDefs | Where-Object { $_.SysName -eq $name }
-			$propDefIds[$propDef.Id] = $propDef.DispName
-		}
-		$srchCond.PropDefId = $propDef.Id
-		$srchCond.SrchOper = 3
-		$srchCond.SrchTxt = $mVal
-		$srchCond.PropTyp = [Autodesk.Connectivity.WebServices.PropertySearchType]::SingleProperty
-		$srchCond.SrchRule = [Autodesk.Connectivity.WebServices.SearchRuleType]::Must
-		$srchConds[0] = $srchCond
-		$srchSort = New-Object autodesk.Connectivity.WebServices.SrchSort
-		$searchStatus = New-Object autodesk.Connectivity.WebServices.SrchStatus
-		$bookmark = ""
-		$mSearchResult = $vault.DocumentService.FindFilesBySearchConditions($srchConds,$null, $null,$true,$true,[ref]$bookmark,[ref]$searchStatus)
+	$srchCond = New-Object autodesk.Connectivity.WebServices.SrchCond
+	$propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
+	$propNames = @("Name")
+	$propDefIds = @{}
+	foreach($name in $propNames) {
+		$propDef = $propDefs | Where-Object { $_.SysName -eq $name }
+		$propDefIds[$propDef.Id] = $propDef.DispName
+	}
+	$srchCond.PropDefId = $propDef.Id
+	$srchCond.SrchOper = 3
+	$srchCond.SrchTxt = $mVal
+	$srchCond.PropTyp = [Autodesk.Connectivity.WebServices.PropertySearchType]::SingleProperty
+	$srchCond.SrchRule = [Autodesk.Connectivity.WebServices.SearchRuleType]::Must
+	$srchConds[0] = $srchCond
+	$srchSort = New-Object autodesk.Connectivity.WebServices.SrchSort
+	$searchStatus = New-Object autodesk.Connectivity.WebServices.SrchStatus
+	$bookmark = ""
+$mSearchResult = $vault.DocumentService.FindFilesBySearchConditions($srchConds,$null, $null,$true,$true,[ref]$bookmark,[ref]$searchStatus)
 
-		If (!$mSearchResult) 
-		{ 
-			[System.Windows.MessageBox]::Show("Datei in '" + $_ReplacedByFilePropDispName + "' konnte nicht gefunden werden.", "Vault Datei-Extras")
-			return
-		}
+If (!$mSearchResult) 
+{ 
+	[System.Windows.MessageBox]::Show([String]::Format($UIString["ADSK-GoToSource_MSG01"], $mVal, $_ReplacedByFilePropDispName), $UIString["ADSK-GoToSource_MNU00"])
+	return
+}
 
-		$folderId = $mSearchResult[0].FolderId
-		$fileName = $mSearchResult[0].Name
+$folderId = $mSearchResult[0].FolderId
+$fileName = $mSearchResult[0].Name
 
-		$folder = $vault.DocumentService.GetFolderById($folderId)
-		$path=$folder.FullName+"/"+$fileName
+$folder = $vault.DocumentService.GetFolderById($folderId)
+$path=$folder.FullName+"/"+$fileName
 
-	$selectionId = [Autodesk.Connectivity.Explorer.Extensibility.SelectionTypeId]::File
-	$location = New-Object Autodesk.Connectivity.Explorer.Extensibility.LocationContext $selectionId, $path
-	$vaultContext.GoToLocation = $location
+$selectionId = [Autodesk.Connectivity.Explorer.Extensibility.SelectionTypeId]::File
+$location = New-Object Autodesk.Connectivity.Explorer.Extensibility.LocationContext $selectionId, $path
+$vaultContext.GoToLocation = $location
--- VLT-MKDE-2018/Vault.Custom/addinVault/QuickstartExtension.ps1
+++ VLT-MKDE-2018/Vault.Custom/addinVault/QuickstartExtension.ps1
@@ -10,7 +10,7 @@
 #=============================================================================#
 #endregion
 
-# Version Info - VDS Quickstart Extension 2018.0.3
+# Version Info - VDS Quickstart Extension 2018.0.4
 
 #retrieve property value given by displayname from folder (ID)
 function mGetFolderPropValue ([Int64] $mFldID, [STRING] $mDispName)
@@ -118,3 +118,66 @@ function mGetFolderNumber($_FileNumber, $_nChar)
 	return $_ItemFilePath
 
 } #end function mGetFolderNumber
+
+
+# VDS Dialogs and Tabs share UIString according DSLanguage.xml override or default powerShell UI culture;
+# VDS MenuCommand scripts don't read as a default; call this function in case $UIString[] key value pairs are needed
+function mGetUIStrings
+{
+	# check language override settings of VDS
+	[xml]$mDSLangFile = Get-Content "C:\ProgramData\Autodesk\Vault 2018\Extensions\DataStandard\Vault\DSLanguages.xml"
+	$mUICodes = $mDSLangFile.SelectNodes("/DSLanguages/Language_Code")
+	$mLCode = @{}
+	Foreach ($xmlAttr in $mUICodes)
+	{
+		$mKey = $xmlAttr.ID
+		$mValue = $xmlAttr.InnerXML
+		$mLCode.Add($mKey, $mValue)
+	}
+	#If override exists, apply it, else continue with $PSUICulture
+	If ($mLCode["UI"]){
+		$mVdsUi = $mLCode["UI"]
+	} 
+	Else{$mVdsUi=$PSUICulture}
+	[xml]$mUIStrFile = get-content ("C:\ProgramData\Autodesk\Vault 2018\Extensions\DataStandard\" + $mVdsUi + "\UIStrings.xml")
+	$UIString = @{}
+	$xmlUIStrs = $mUIStrFile.SelectNodes("/UIStrings/UIString")
+	Foreach ($xmlAttr in $xmlUIStrs) {
+		$mKey = $xmlAttr.ID
+		$mValue = $xmlAttr.InnerXML
+		$UIString.Add($mKey, $mValue)
+		}
+	return $UIString
+}
+
+# VDS Dialogs and Tabs share property name translations $Prop[_XLTN_*] according DSLanguage.xml override or default powerShell UI culture;
+# VDS MenuCommand scripts don't read as a default; call this function in case $UIString[] key value pairs are needed
+function mGetPropTranslations
+{
+	# check language override settings of VDS
+	[xml]$mDSLangFile = Get-Content "C:\ProgramData\Autodesk\Vault 2018\Extensions\DataStandard\Vault\DSLanguages.xml"
+	$mUICodes = $mDSLangFile.SelectNodes("/DSLanguages/Language_Code")
+	$mLCode = @{}
+	Foreach ($xmlAttr in $mUICodes)
+	{
+		$mKey = $xmlAttr.ID
+		$mValue = $xmlAttr.InnerXML
+		$mLCode.Add($mKey, $mValue)
+	}
+	#If override exists, apply it, else continue with $PSUICulture
+	If ($mLCode["DB"]){
+		$mVdsDb = $mLCode["DB"]
+	} 
+	Else{
+		$mVdsUi=$PSUICulture
+	}
+	[xml]$mPrpTrnsltnFile = get-content ("C:\ProgramData\Autodesk\Vault 2018\Extensions\DataStandard\" + $mVdsDb + "\PropertyTranslations.xml")
+	$mPrpTrnsltns = @{}
+	$xmlPrpTrnsltns = $mPrpTrnsltnFile.SelectNodes("/PropertyTranslations/PropertyTranslation")
+	Foreach ($xmlAttr in $xmlPrpTrnsltns) {
+		$mKey = $xmlAttr.Name
+		$mValue = $xmlAttr.InnerXML
+		$mPrpTrnsltns.Add($mKey, $mValue)
+		}
+	return $mPrpTrnsltns
+}
